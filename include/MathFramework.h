06cf3880999da2568ff7961f60f7ca808a9e9835
vovatrykoz <vovatrykoz@gmail.com>
2024-11-13 16:17:23 +0100

    document att function

--- include/MathFramework.h
+++ include/MathFramework.h
@@ -6,9 +6,17 @@
 // Contains functions from section 3.4 Mathematical Framework
 
 /**
+ * @brief Checks if "activation time travel" occurs between two tasks.
+ *
  * The following boolean function att returns true if “activation
  * time travel” occurs (i.e. when the reader is activated
  * before the writer), and false if it doesn’t
+ *
+ * @param writerTask The task instance representing the writer task.
+ * @param readerTask The task instance representing the reader task.
+ * @return true if activation time travel occurs (reader activates before
+ * writer).
+ * @return false if activation time travel does not occur.
  */
 bool att(const TaskInstance& writerTask, const TaskInstance& readerTask);
 
