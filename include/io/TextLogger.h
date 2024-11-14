5af5773f4fd759a5da8da7a623cca47960242575
vovatrykoz <vovatrykoz@gmail.com>
2024-11-14 19:37:39 +0100

    create text logger interface

---
+++ include/io/TextLogger.h
@@ -0,0 +1,17 @@
+#ifndef _TEXT_LOGGER_H_
+#define _TEXT_LOGGER_H_
+
+#include <TimedPath.h>
+
+#include <optional>
+#include <set>
+
+class TextLogger {
+public:
+    void logResults(const std::set<TimedPath>& allPathsSet,
+                    const std::set<TimedPath>& validPathSet,
+                    const std::set<TimedPath>& invalidPathSet,
+                    const std::optional<TimedPath>& maximumLatencyPath) const;
+};
+
+#endif
