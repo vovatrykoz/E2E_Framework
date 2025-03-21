f8a46942cb658ade2f4b39e20ffad8284305c3e2
vovatrykoz <vovatrykoz@gmail.com>
2025-03-21 10:36:09 +0100

    use c++ style for attributes rather than the C style

--- include/MathFramework.h
+++ include/MathFramework.h
@@ -26,8 +26,9 @@ namespace mathframework {
  * writer).
  * @return false if activation time travel does not occur.
  */
+[[gnu::const]]
 bool att(PeriodicTaskInstance writerTaskInstance,
-         PeriodicTaskInstance readerTaskInstance) __attribute__((const));
+         PeriodicTaskInstance readerTaskInstance);
 
 /**
  * @brief Determines if the writer and reader tasks overlap in execution.
@@ -41,8 +42,9 @@ bool att(PeriodicTaskInstance writerTaskInstance,
  * @return true if there is an overlap in execution between writer and reader.
  * @return false if there is no overlap in execution.
  */
+[[gnu::const]]
 bool crit(PeriodicTaskInstance writerTaskInstance,
-          PeriodicTaskInstance readerTaskInstance) __attribute__((const));
+          PeriodicTaskInstance readerTaskInstance);
 
 /**
  * @brief Determines if the reader task must wait due to the writer's priority.
@@ -57,8 +59,8 @@ bool crit(PeriodicTaskInstance writerTaskInstance,
  * priority.
  * @return false if the reader does not need to wait for the writer.
  */
-bool wait(PeriodicTask writerTask, PeriodicTask readerTask)
-    __attribute__((const));
+[[gnu::const]]
+bool wait(PeriodicTask writerTask, PeriodicTask readerTask);
 
 /**
  * @brief Determines the forward reachability between two task instances.
@@ -74,8 +76,9 @@ bool wait(PeriodicTask writerTask, PeriodicTask readerTask)
  * @return true if forward reachability conditions are met.
  * @return false if forward reachability conditions are not met.
  */
+[[gnu::const]]
 bool forw(PeriodicTaskInstance writerTaskInstance,
-          PeriodicTaskInstance readerTaskInstance) __attribute__((const));
+          PeriodicTaskInstance readerTaskInstance);
 
 /**
  * @brief Checks if a writer task instance can reach a reader task instance,
@@ -96,9 +99,10 @@ bool forw(PeriodicTaskInstance writerTaskInstance,
  * @return false if it cannot reach the reader task instance, indicating an
  * overwrite.
  */
+[[gnu::const]]
 bool reach(PeriodicTaskInstance currentWriterTaskInstance,
            PeriodicTaskInstance readerTaskInstance,
-           PeriodicTaskInstance nextWriterTaskInstance) __attribute__((const));
+           PeriodicTaskInstance nextWriterTaskInstance);
 
 /**
  * @brief Determines reachability for an entire timed path of task instances.
@@ -114,8 +118,8 @@ bool reach(PeriodicTaskInstance currentWriterTaskInstance,
  * @return false if any two consecutive task instances in the path are not
  * reachable.
  */
-bool pathReach(const std::vector<PeriodicTaskInstance>& timedPath)
-    __attribute__((pure));
+[[gnu::pure]]
+bool pathReach(const std::vector<PeriodicTaskInstance>& timedPath);
 
 }  // namespace mathframework
 }  // namespace e2e
