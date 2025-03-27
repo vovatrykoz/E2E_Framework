33f32ef12a25a11d6076a4e02fa7383d241adabc
vovatrykoz <vovatrykoz@gmail.com>
2025-03-27 15:56:47 +0100

    add const modifiers to the content of ConsoleSystemLogger::currentTime

--- source/io/logger/ConsoleSystemLogger.cpp
+++ source/io/logger/ConsoleSystemLogger.cpp
@@ -55,9 +55,9 @@ void e2e::io::ConsoleSystemLogger::logMessage(
 }
 
 std::string ConsoleSystemLogger::currentTime() {
-    auto now = std::chrono::system_clock::now();
-    std::time_t now_time_t = std::chrono::system_clock::to_time_t(now);
-    std::tm local_tm = *std::localtime(&now_time_t);
+    const auto now = std::chrono::system_clock::now();
+    const std::time_t now_time_t = std::chrono::system_clock::to_time_t(now);
+    const std::tm local_tm = *std::localtime(&now_time_t);
 
     std::ostringstream oss;
     oss << std::put_time(&local_tm, "%Y-%m-%d %H:%M:%S");
