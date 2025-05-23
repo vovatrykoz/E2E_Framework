d5c69a4a5a71b0335e86288a565fc75402c1344a
vovatrykoz <vovatrykoz@gmail.com>
2025-05-23 17:01:37 +0200

    add comments to ISystemLogger

--- include/e2e/io/logger/ISystemLogger.h
+++ include/e2e/io/logger/ISystemLogger.h
@@ -3,23 +3,45 @@
 
 #include <string>
 
+/**
+ * @brief Interface for a system logger that provides methods for logging
+ *        information, warnings, errors, and raw messages.
+ */
 class ISystemLogger {
-   public:
+public:
+    /**
+     * @brief Logs an informational message.
+     *
+     * @param infoMessage The message to be logged as information.
+     */
     virtual void logInfo(const std::string& infoMessage) const = 0;
 
+    /**
+     * @brief Logs a warning message.
+     *
+     * @param warningMessage The message to be logged as a warning.
+     */
     virtual void logWarning(const std::string& warningMessage) const = 0;
 
+    /**
+     * @brief Logs an error message.
+     *
+     * @param errorMessage The message to be logged as an error.
+     */
     virtual void logError(const std::string& errorMessage) const = 0;
 
     /**
-     * @brief logs a message
+     * @brief Logs a raw message.
      *
      * Effectively the same as `logInfo`, except it doesn't prefix the message
      * with the "INFO:" string and current time, logging the message exactly as
      * passed. Keep in mind that this method will also not output any newline
      * character after the message has been logged, unlike all other logging
      * methods. The newline character should be included as a part of the
-     * message if it is neccesarry
+     * message if it is necessary.
+     *
+     * @param message The exact message to be logged, including any newline
+     * characters if needed.
      */
     virtual void logMessage(const std::string& message) const = 0;
 
